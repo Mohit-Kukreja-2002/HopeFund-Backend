e17c5896bd902e4bedfe86204223af4f8e7ebb43
Mohit-Kukreja-2002 <93993902+Mohit-Kukreja-2002@users.noreply.github.com>
2024-02-08 16:09:18 +0530

    cron but via web

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ import  {app}  from "./app.js";
 import connectDb from "./utils/db.js";
 import {v2 as cloudinary} from "cloudinary"
 import dotenv from "dotenv"
-import cron from 'node-cron';
+// import cron from 'node-cron';
 dotenv.config();
 
-const keepServerActive = () => {
-    // Perform any task here that keeps the server active
-    console.log('Task executed to keep server active.');
-};
+// const keepServerActive = () => {
+//     // Perform any task here that keeps the server active
+//     console.log('Task executed to keep server active.');
+// };
 
-cron.schedule('*/10 * * * *', keepServerActive);
+// cron.schedule('*/10 * * * *', keepServerActive);
 
 // cloudinery config
 cloudinary.config({
